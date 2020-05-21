5b04c13e0c5b59b0495f52ea84d25ff0bc66c51a
Kevin <terrykevin1@gmail.com>
2020-05-20 21:19:54 -0400

    Updates

---
+++ precache-manifest.7eeefe3aebde49b774e9efad4cc68cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20827c23555df0d28307892ae3ac75d2",
+    "url": "/PicSome/index.html"
+  },
+  {
+    "revision": "688b41a4263c25f039a0",
+    "url": "/PicSome/static/css/main.ca6de698.chunk.css"
+  },
+  {
+    "revision": "b87b73afeaa9588d7ca4",
+    "url": "/PicSome/static/js/2.b2a9b388.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/PicSome/static/js/2.b2a9b388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688b41a4263c25f039a0",
+    "url": "/PicSome/static/js/main.bcbd5096.chunk.js"
+  },
+  {
+    "revision": "dd989b4efa29cadd4380",
+    "url": "/PicSome/static/js/runtime-main.35782b80.js"
+  }
+]);
